c545fe6d1826bea38ec7f4ac9e9983cb96f6aa2a
Afonso <afonsobezerra199@gmail.com>
2022-12-09 11:17:07 -0300

    fix overflow

--- src/components/sections/index/body/index.tsx
+++ src/components/sections/index/body/index.tsx
@@ -9,7 +9,7 @@ const Body = () => {
   const {active,activeProfile,setActive, setProfile} = useLayoutContextSide();
   const {openNotify, setOpenNotify} = useLayoutContextNotification()
   return (
-    <BodyStyle arial-hidden onClick={() => {
+    <BodyStyle style={active || activeProfile ? { overflow: 'hidden'  } : {} } arial-hidden onClick={() => {
       active && setActive(false)
       activeProfile && setProfile(false)
       openNotify && setOpenNotify(false)
