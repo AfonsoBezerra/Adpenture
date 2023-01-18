791497dde094b6692e96e0a639ac57aafb9ca846
Afonso <afonsobezerra199@gmail.com>
2023-01-18 13:14:16 -0300

    map size

--- src/components/mapBox/style.ts
+++ src/components/mapBox/style.ts
@@ -2,12 +2,15 @@ import styled from 'styled-components';
 
 export const MapBoxStyle = styled.div`
     width: 100%;
-    height: 100%;
+    height:50%;
     background-color: white;
     border-radius: 10px;
     padding: 1rem;
+    @media (min-width: 1919px){
+        height: 70%;
+    }
     @media (max-width: 1440px){
-        height: 50%;
+        height: 40%;
     }
     @media (max-width: 600px){
         height: 600px;
